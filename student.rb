dceed417d92854d2cae4739376fc66e4512793a7
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-02 10:47:55 -0500

    Implement has-many/belongs relantionship between Classroom and Student class.

--- student.rb
+++ student.rb
@@ -1,9 +1,15 @@
 require_relative 'person'
 
 class Student < Person
-  def initialize(age, name, classroom, parent_permission = DEFAULT_BOOL)
+  def initialize(age, name, parent_permission = DEFAULT_BOOL)
     super(age, name, parent_permission)
+  end
+
+  attr_accessor :classroom
+
+  def classroom=(classroom)
     @classroom = classroom
+    classroom.students.push(self) unless classroom.students.include?(self)
   end
 
   def play_hooky
