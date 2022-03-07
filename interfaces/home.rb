a5d402ef0dc670f241a0efba050de0fc24a75eb5
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-07 16:09:47 -0500

    Reorganize the estructure of rentals methods into another file.

--- interfaces/home.rb
+++ interfaces/home.rb
@@ -3,20 +3,17 @@ require_relative 'persons'
 require_relative 'rentals'
 require_relative 'tui'
 
-class App
-  MAIN_INPUT_MESSAGE = 'Enter an option number here --> '.freeze
-  MAIN_PRESSENTER_MESSAGE = 'Press ENTER to continue'.freeze 
+class App   
   include Tui
 
   def initialize
     @options_answer = 0
     @books = Books.new
     @persons = Persons.new
+    @rentals = Rentals.new
     clear_screen
     main_greeting_message
     progress_bar
-    #@rentals = Rentals.new
-
   end
 
   def show_main_options
@@ -47,13 +44,12 @@ class App
       puts
     when 3
       @persons.create_person
-
     when 4
       @books.create_book
     when 5
-      #create_rental
+      @rentals.create_rental(@books.books, @persons.persons)
     when 6
-      #show_all_rentals
+      @rentals.show_all_rentals(@persons.persons)
     else
       puts 'Exiting session'
       loading_effect
@@ -72,39 +68,5 @@ class App
       option_selected
     end
   end
-
-  def create_rental
-    puts 'Select a book from the following list:'
-    @books.each_with_index { |book, index| puts "#{index}) Title: #{book.title}, Author: #{book.author}" }
-    print MAIN_INPUT_MESSAGE
-    book_index = gets.chomp.to_i
-    puts
-    puts 'Select a person form the following list:'
-    @persons.each_with_index do |person, index|
-      puts "#{index}) Name: #{person.name}, Age: #{person.age}, ID: #{person.id}"
-    end
-    print MAIN_INPUT_MESSAGE
-    person_index = gets.chomp.to_i
-    puts
-    print 'Enter date of retrieval --> '
-    date = gets.chomp
-    Rental.new(date, @books[book_index], @persons[person_index])
-    puts 'Rental created successfully!'
-    puts
-  end
-
-  def show_all_rentals
-    print 'Enter the person\'s ID --> '
-    person_id = gets.chomp.to_i
-    renter = @persons.select { |person| person.id == person_id }
-    puts
-    puts "Rentals of #{renter.first.name}:"
-    puts 'EMPTY' if renter.first.rentals.empty?
-    puts(renter.first.rentals.map { |rental| "Book: #{rental.book.title}, Rented on: #{rental.date}" })
-    print MAIN_PRESSENTER_MESSAGE
-    gets
-    puts
-  end
-
 
 end
