7e4e6bef05e8fbde54b20e456a86e52c961ba54c
TheLuisAbarca <arclaav.adm@gmail.com>
2022-03-09 11:15:11 -0500

    Implement the corresponding tests for book class.

---
+++ spec/book_spec.rb
@@ -0,0 +1,19 @@
+require_relative 'require_helper'
+
+RSpec.describe Book do
+    before :each do
+      @book = Book.new('TLOTR', 'Tolkien')
+    end
+
+    it 'Takes two parameters and returns a Book object' do
+      expect(@book).to be_an_instance_of Book
+    end
+  
+    it 'Check title of the book' do
+      expect(@book.title).to eql 'TLOTR'
+    end
+  
+    it 'Check author of the book' do
+      expect(@book.author).to eql 'Tolkien'
+    end
+end
